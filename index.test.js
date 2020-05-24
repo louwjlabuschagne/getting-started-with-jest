fc3de31ed597c760110a8f1e5061aa56aeecccb5
louwjlab <louwjlabuschagne@gmail.com>
2020-05-24 13:27:41 +0200

    More files

---
+++ index.test.js
@@ -0,0 +1,8 @@
+const getAboutUsLink = require("./index");
+test("Returns about-us for english language", () => {
+    expect(getAboutUsLink("en-US")).toBe("/about-us");
+});
+
+test.only("Returns about-us for spanish language", () => {
+    expect(getAboutUsLink("es-ES")).toBe("/acerca-de");
+});
